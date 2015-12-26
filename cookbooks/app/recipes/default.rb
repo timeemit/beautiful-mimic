73a078b469849269a65904c1d5d8898c6af25279
TimeEmit <LiamNorris1231853211@gmail.com>
2015-12-26 01:45:48 -0500

    Recipe for provisioning sinatra on unicorn

---
+++ cookbooks/app/recipes/default.rb
@@ -0,0 +1,41 @@
+file '/var/log/unicorn.stdout.log' do
+  user 'ubuntu'
+  action :touch
+end
+
+file '/var/log/unicorn.stderr.log' do
+  user 'ubuntu'
+  action :touch
+end
+
+execute 'install keyserver' do
+  command 'su ubuntu -c "gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3"'
+  not_if 'which rvm'
+end
+
+execute 'install rvm' do
+  command 'su ubuntu -c "curl -sSL https://get.rvm.io | bash -s stable"'
+  not_if 'su ubuntu -l -c "which rvm"'
+end
+
+execute 'install ruby version' do
+  command 'su ubuntu -l -c "rvm install `cat /opt/code/.ruby-version`"'
+end
+
+execute 'install gemset' do
+  command 'su ubuntu -l -c "rvm use `cat /opt/code/.ruby-version` && rvm gemset create `cat /opt/code/.ruby-gemset`"'
+end
+
+execute 'install gems' do
+  command 'su ubuntu -l -c "cd /opt/code/ && bundle install"'
+end
+
+execute 'start unicorn' do
+  command 'su ubuntu -l -c "cd /opt/code/app/ && bundle exec unicorn -c /opt/code/app/unicorn.conf.rb"'
+  not_if '[ -f /tmp/app.pid ]'
+end
+
+execute 'restart unicorn' do
+  command 'su ubuntu -l -c "kill -s 1 `cat /tmp/app.pid`"' # Signal 1 is SIGHUP
+  only_if '[ -f /tmp/app.pid ]'
+end
