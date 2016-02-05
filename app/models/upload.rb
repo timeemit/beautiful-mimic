69c804bc07a4cf390740ac8789651fed8babf6c5
TimeEmit <LiamNorris1231853211@gmail.com>
2016-02-05 00:36:08 -0800

    Validate uploads for user hash

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -6,6 +6,7 @@ class Upload
   field :filename, type: String # name of file --> used to look up in S3
   field :file_hash, type: String # SHA256 of file
 
+  validates :user_hash, presence: true
   validates :filename, presence: true, length: { maximum: 256 }
   validates :file_hash, presence: true, uniqueness: true
 
