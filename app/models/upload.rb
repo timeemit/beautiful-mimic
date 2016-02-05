b8130a35185682ea95cb9164231af2fbb51e6491
TimeEmit <LiamNorris1231853211@gmail.com>
2016-02-05 00:40:24 -0800

    Allow for `user_hash` to be absent

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -6,8 +6,8 @@ class Upload
   field :filename, type: String # name of file --> used to look up in S3
   field :file_hash, type: String # SHA256 of file
 
-  validates :user_hash, presence: true
+  validates :user_hash, presence: false # No user has == available to the entire system
   validates :filename, presence: true, length: { maximum: 256 }
-  validates :file_hash, presence: true, uniqueness: true
+  validates :file_hash, presence: true, uniqueness: { scope: :user_hash }
 
 end
