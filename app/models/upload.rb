bc7faf039bd34563818f866d63ad990bac11a8c6
TimeEmit <LiamNorris1231853211@gmail.com>
2016-02-14 21:35:28 -0800

    Upload model now enforces a more strict filename regex

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -3,14 +3,14 @@ class Upload
   include Mongoid::Timestamps::Created
 
   field :user_hash, type: String # foreign key to user; system-wide upload if ABSENT
-  field :filename, type: String # name of file --> used to look up in S3
-  field :file_hash, type: String # SHA256 of file
+  field :file_hash, type: String # SHA256 of file --> used to look up in S3
+  field :filename, type: String # name of file
 
   has_many :content_mimics, class_name: 'Mimic', inverse_of: :content_upload
   has_many :style_mimics, class_name: 'Mimic', inverse_of: :style_upload
 
   validates :user_hash, presence: false # No user hash == available to the entire system
-  validates :filename, presence: true, length: { maximum: 256 }
+  validates :filename, presence: true, length: { maximum: 256 }, format: /(\.jpg|\.jpeg|\.png|\.tiff)\z/
   validates :file_hash, presence: true, uniqueness: { scope: :user_hash }
 
 end
--- app/spec/models/upload_spec.rb
+++ app/spec/models/upload_spec.rb
@@ -1,10 +1,25 @@
 require_relative '../spec_base'
 
 describe Upload do
+  let(:valid_upload) { Upload.new(filename: 'marilyn-monroe.jpg', file_hash: 'aaaa0000') }
+
+  it 'can be valid' do
+    expect(valid_upload.valid?).to be true
+  end
+
   it 'requires all fields' do
     upload = Upload.new
     expect(upload.valid?).to be false
-    expect(upload.errors.messages[:filename]).to eql ["can't be blank"]
+    expect(upload.errors.messages[:filename]).to eql ["can't be blank", "is invalid"]
     expect(upload.errors.messages[:file_hash]).to eql ["can't be blank"]
+    expect(upload.errors.messages[:user_hash]).to be nil
+  end
+
+  it 'needs to have an appropriate file extension' do
+    upload = valid_upload
+    upload.filename = 'marilyn-monroe.exe' # Oh noes!
+    expect( upload.valid? ).to be false
+    expect(upload.errors.messages[:filename]).to eql ["is invalid"]
+    expect(upload.errors.messages[:user_hash]).to be nil
   end
 end
