8fbefd01b3f0f5f72353f9e6af00af58073bb56c
TimeEmit <LiamNorris1231853211@gmail.com>
2016-03-06 17:20:42 -0800

    Spec fixes

--- app/spec/routes/files/get_spec.rb
+++ app/spec/routes/files/get_spec.rb
@@ -9,7 +9,10 @@ describe 'GET /files' do
   let(:expected_url_prefix) { "#{s3_url}/#{bucket}" }
 
   it 'Can return the uploaded file thumb' do
+    Upload.create!(user_hash: user_hash, filename: 'redpill.png', file_hash: file_hash)
+
     get "/files/#{file_hash}", {}, {'rack.session' => {user_hash: user_hash}}
+
     expect(last_response.status).to eq 302
     expect(last_response.body).to eq ''
     expect(last_response.headers['Location']).to start_with expected_url_prefix
@@ -18,7 +21,10 @@ describe 'GET /files' do
   end
 
   it 'Can return the originally uploaded file' do
+    Upload.create!(user_hash: user_hash, filename: 'redpill.png', file_hash: file_hash)
+
     get "/files/#{file_hash}", {style: 'original'}, {'rack.session' => {user_hash: user_hash}}
+
     expect(last_response.status).to eq 302
     expect(last_response.body).to eq ''
     expect(last_response.headers['Location']).to start_with expected_url_prefix
@@ -26,8 +32,9 @@ describe 'GET /files' do
     expect(last_response.headers['Location']).to_not include 'thumb'
   end
 
-  it 'Can return the originally uploaded system file' do
+  it 'Can return the original system file' do
     get "/files/#{file_hash}", {system: 'true'}, {'rack.session' => {user_hash: user_hash}}
+
     expect(last_response.status).to eq 302
     expect(last_response.body).to eq ''
     expect(last_response.headers['Location']).to start_with expected_url_prefix
@@ -35,8 +42,9 @@ describe 'GET /files' do
     expect(last_response.headers['Location']).to_not include 'original'
   end
 
-  it 'Can return the originally uploaded system file' do
+  it 'Can return the original system file' do
     get "/files/#{file_hash}", {style: 'original', system: 'true'}, {'rack.session' => {user_hash: user_hash}}
+
     expect(last_response.status).to eq 302
     expect(last_response.body).to eq ''
     expect(last_response.headers['Location']).to start_with expected_url_prefix
