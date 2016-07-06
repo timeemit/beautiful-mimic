f93ed488fb85203eefc4587031a921795a38216a
TimeEmit <LiamNorris1231853211@gmail.com>
2016-07-05 19:38:56 -0700

    Ansible playbooks!

--- ops/knife.rb
+++ ops/knife.rb
@@ -1,4 +1,4 @@
 chef_server_url   'http://127.0.0.1:8889'
 node_name         'compute'
-client_key        '~/.ssh/id_rsa'
+client_key        '~/.ssh/beautiful-mimic.pem'
 cookbook_path     './cookbooks'
