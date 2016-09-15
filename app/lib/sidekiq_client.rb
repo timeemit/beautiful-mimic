98496cb72d423d4de8094b66785ae6e9da3772f6
TimeEmit <LiamNorris1231853211@gmail.com>
2016-09-15 00:23:30 -0700

    Redis connection with auth and w/o db

--- app/lib/sidekiq_client.rb
+++ app/lib/sidekiq_client.rb
@@ -3,7 +3,11 @@ require 'sidekiq'
 class SidekiqClient
   def self.connect!(env)
     redis = env['redis']
-    url = "#{redis['ip']}:#{redis['port']}/#{redis['db']}"
+    url = "#{redis['ip']}:#{redis['port']}"
+
+    if env['redis']['password']
+      url = "x:#{env['redis']['password']}@#{url}"
+    end
 
     Sidekiq.configure_client do |config|
       config.redis = { url: "redis://#{url}" }
