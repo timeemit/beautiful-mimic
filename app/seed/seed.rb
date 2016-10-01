dfebb9eda3cd0f88aced54c23f2ce872cc133563
TimeEmit <LiamNorris1231853211@gmail.com>
2016-09-30 20:45:31 -0700

    App: Bring seed script up to speed.

--- app/seed/seed.rb
+++ app/seed/seed.rb
@@ -1,6 +1,13 @@
 require 'yaml'
 require 'mongoid'
+require 'mini_magick'
+
+require_relative '../lib/secret'
 require_relative '../lib/aws_authenticator'
+require_relative '../lib/model'
+require_relative '../lib/s3_upload'
+require_relative '../lib/s3_upload/image'
+require_relative '../models/upload'
 require_relative '../models/uploader'
 
 environments = ['development', 'test', 'production']
@@ -8,13 +15,12 @@ environment = ARGV[0]
 raise "Choose one of #{environments}" unless environments.include? environment
 
 # Collect args
-environment_path = File.expand_path("environments/#{environment}.yml", "#{__dir__}/..")
-env = YAML.load_file(environment_path)
+Secret.set!(environment)
 filepaths = Dir["#{__dir__}/images/*"]
 
 # Initialize
-Mongoid.load!(environment_path, 'mongo')
-AwsAuthenticator.authenticate!(env)
+Mongoid.load!(Secret.path, 'mongo')
+AwsAuthenticator.authenticate!(Secret.config)
 
 # Execute
 p "Uploading for environment: #{environment}"
@@ -25,14 +31,13 @@ filepaths.each do |filepath|
   p "Uploading: #{filename}"
 
   uploader = Uploader.new(
-    env['S3']['bucket'],
     nil,
     filename,
     File.new(filepath)
   )
   unless uploader.save!
+    p "Uploading: Error: #{filename}!"
     p uploader.s3_upload.errors
     p uploader.upload.errors
-    raise "Uploading: #{filename}: Invalid!"
   end
 end
