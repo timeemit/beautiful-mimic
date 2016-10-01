5d266e5266a712eb17bb27b05c23ec016a7da62f
TimeEmit <LiamNorris1231853211@gmail.com>
2016-09-30 23:31:10 -0700

    App: Workers: Add aws dependency to sidekiq server

--- app/workers/server.rb
+++ app/workers/server.rb
@@ -5,18 +5,20 @@ require 'mongoid'
 require 'mini_magick'
 require 'open3' # for process execution
 
-require_relative '../models/upload'
 require_relative '../lib/secret'
+require_relative '../lib/aws_authenticator'
 require_relative '../lib/model'
 require_relative '../lib/s3_upload'
 require_relative '../lib/s3_upload/image'
 require_relative '../lib/s3_upload/trained_model'
 require_relative '../models/mimic'
+require_relative '../models/upload'
 require_relative './mimic_maker'
 
 Secret.set! ENV['SIDEKIQ_ENV']
 
 Mongoid.load!(Secret.path, 'mongo')
+AwsAuthenticator.authenticate!(Secret.config)
 
 redis = Secret.config['redis']
 url = "#{redis['ip']}:#{redis['port']}/#{redis['db']}"
