08b62a60bba350b5cff991cdf50c0d4d02f5b13b
TimeEmit <LiamNorris1231853211@gmail.com>
2016-10-02 21:51:15 -0700

    App: MimicImg: Proudly display the original image

--- app/src/app/mimic-img.js
+++ app/src/app/mimic-img.js
@@ -3,7 +3,7 @@ var MimicImg = React.createClass({
     var mimic = this.props.mimic;
 
     if ( mimic.mimic_hash ) {
-      return ( <img className='pure-img center' src={'/files/' + mimic.mimic_hash}></img> );
+      return ( <img className='pure-img center' src={'/files/' + mimic.mimic_hash + '?style=original'}></img> );
     } else {
       return ( <img className='pure-img center rotate' src='/images/logo-yellow.png'></img> );
     }
