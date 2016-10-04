fb2448d5cf2a3e044333c4c1d2ae865312086801
TimeEmit <LiamNorris1231853211@gmail.com>
2016-10-03 22:58:43 -0700

    App: Mimic.js: Add navigation buttons

--- app/src/app/mimic.js
+++ app/src/app/mimic.js
@@ -13,13 +13,24 @@ var MimicShow = React.createClass({
     }
     return (
       <div>
-        <div className='pure-u-1'>
+        <div className='pure-u-2-5 page-break-mini'>
+          <a href='/' className='pure-button center'>
+            <h2 className='center-text'><i className='fa fa-2x fa-angle-left'></i></h2>
+          </a>
+        </div>
+        <div className='pure-u-1-5'></div>
+        <div className='pure-u-2-5 page-break-mini'>
+          <a href='/mimics/new' className='pure-button center'>
+            <h2 className='center-text'><i className='fa fa-2x'>+</i></h2>
+          </a>
+        </div>
+        <div className='pure-u-1 page-break'>
           <MimicImg mimic={mimic} />
         </div>
-        <div className='pure-u-1-2 margin-above'>
+        <div className='pure-u-1-2 page-break'>
           <img className='pure-img center' src={ '/files/' + mimic.content_hash + '?style=original' }></img>
         </div>
-        <div className='pure-u-1-2 margin-above'>
+        <div className='pure-u-1-2 page-break'>
           <img className='pure-img center' src={ '/files/' + mimic.style_hash + '?style=original' }></img>
         </div>
         { print_button }
