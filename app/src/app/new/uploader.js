35496f16c7dc6949aa74b3ca50832b92aaa6e6d5
TimeEmit <LiamNorris1231853211@gmail.com>
2016-10-05 21:09:30 -0700

    App: Uploder.js: Indicate loading of an image

--- app/src/app/new/uploader.js
+++ app/src/app/new/uploader.js
@@ -1,7 +1,13 @@
 var Uploader = React.createClass({
+  getInitialState: function() {
+    return {
+      uploading: false,
+    };
+  },
+
   submit: function(e){
     var form_data = new FormData();
-
+    this.setState({uploading: true});
     form_data.append('file', e.target.files[0]);
 
     $.post({
@@ -11,19 +17,31 @@ var Uploader = React.createClass({
       contentType: false,
       processData: false,
       success: function(response){
+        this.setState({uploading: false});
         this.props.add_upload(JSON.parse(response));
       }.bind(this)
     });
   },
   render: function(){
+    var disabled = '';
+    var pure_disabled = '';
+    var camera = 'fa-camera';
+    var upload = 'fa-upload';
+    if (this.state.uploading) {
+      disabled = 'disabled';
+      pure_disabled = 'pure-button-disabled';
+      camera = 'fa-gear fa-spin';
+      upload = 'fa-gear fa-spin';
+    }
+
     return (
-      <div className='purt-g'>
+      <div className='purt-g .js-submit-css'>
         <div className='pure-u-1-6'></div>
         <div className='pure-u-1-6'>
           <h3 className='file-upload'>
-            <label className='center pure-button'>
+            <label className={'center pure-button ' + pure_disabled}>
               <span>
-                <i className='fa fa-2x fa-camera'></i>
+                <i className={'fa fa-2x ' + camera}></i>
               </span>
               <input type='file' onChange={this.submit} className='upload'></input>
             </label>
@@ -33,9 +51,9 @@ var Uploader = React.createClass({
         <div className='pure-u-1-6'></div>
         <div className='pure-u-1-6'>
           <h3 className='file-upload'>
-            <label className='center pure-button'>
+            <label className={'center pure-button ' + pure_disabled}>
               <span>
-                <i className='fa fa-2x fa-upload'></i>
+                <i className={'fa fa-2x ' + upload}></i>
               </span>
               <input type='file' onChange={this.submit} className='upload'></input>
             </label>
