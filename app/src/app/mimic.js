6ad894a5c735bba776cf68d0caad1e11af6315e1
TimeEmit <LiamNorris1231853211@gmail.com>
2016-10-06 23:02:55 -0700

    App: mimic.js: Make the print button animated

--- app/src/app/mimic.js
+++ app/src/app/mimic.js
@@ -32,7 +32,7 @@ var MimicShow = React.createClass({
     var print_button = null;
     if ( mimic.mimic_hash ) { 
       print_button = (
-        <a href={'/files/' + mimic.mimic_hash + '/print'} className='pure-u-1 pure-button pure-button-primary page-break'>
+        <a href={'/files/' + mimic.mimic_hash + '/print'} className='pure-u-1 pure-button pure-button-primary page-break clickable'>
           <h2 className='center-text'><i className='fa fa-2x fa-print'></i></h2>
         </a>
       )
